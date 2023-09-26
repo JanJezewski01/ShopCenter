9f84e6510ce3b2d0f5c917694ffc8b7c5802fab0
JanJezewski01 <jezewski533@gmail.com>
2023-09-26 22:36:08 +0200

    new el

--- roliny/roliny.component.ts
+++ roliny/roliny.component.ts
@@ -8,10 +8,7 @@ import { Component } from '@angular/core';
 export class RolinyComponent {
   displayAloes = '';
 
-
-  showAloes(){
-    this.displayAloes = 'Ciekawostką o aloesie jest to, że jest to roślina o wielu zastosowaniach leczniczych i kosmetycznych, która była używana przez wiele kultur na całym świecie od tysięcy lat. Jednakże, aby zwiększyć swoją odporność na suche i trudne warunki klimatyczne, aloes jest w stanie "zamknąć" swoje pory, utrzymując wodę w liściach, co skutkuje ich charakterystycznym wyglądem i twardej, grubej skórze.';
-  }
+  
 
 
 }
