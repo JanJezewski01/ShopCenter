4c6932ba0a903285f8ab0d84914c73ddd6794cb5
JanJezewski01 <jezewski533@gmail.com>
2024-02-05 13:28:27 +0100

    new file

--- app.module.ts
+++ app.module.ts
@@ -4,22 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
-
-import { PolecaneComponent } from './polecane/polecane.component';
 import { ButyComponent } from './buty/buty.component';
 import { RolinyComponent } from './roliny/roliny.component';
-import { PaginationComponent } from './pagination/pagination.component';
-
 import { MebleComponent } from './meble/meble.component';
 import { SkarpetyComponent } from './skarpety/skarpety.component';
 import { BluzyComponent } from './bluzy/bluzy.component';
 import { SpodnieComponent } from './spodnie/spodnie.component';
 import { AgdComponent } from './agd/agd.component';
-
 import { HomePageComponent } from './home-page/home-page.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-
 import { LightNightButtonComponent } from './light-night-button/light-night-button.component';
 import { WwComponent } from './ww/ww.component';
 
@@ -31,30 +23,18 @@ import { WwComponent } from './ww/ww.component';
     AppComponent,
     LogoComponent,
   NavbarComponent,
- 
-
-  PolecaneComponent,
   ButyComponent,
   RolinyComponent,
-  PaginationComponent,
-
-
   MebleComponent,
   SkarpetyComponent,
   BluzyComponent,
   SpodnieComponent,
   AgdComponent,
    HomePageComponent,
-  AboutMeComponent,
 
   LightNightButtonComponent,
   WwComponent,
 
-
-  
- 
-
-  
 ],
   imports: [BrowserModule, FormsModule],
   providers: [],
